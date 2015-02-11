e0f2d84116b32e0374559c96d4bced9ab62bcd8a
Bradley C. Kuszmaul <bradley@tokutek.com>
2015-02-10 20:18:38 -0500

    Timers, with statistics

--- timers.cc
+++ timers.cc
@@ -1,10 +1,10 @@
-#define _GNU_SOURCE
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <vector>
 
 double tdiff_timeval(struct timeval *start, struct timeval *end) {
   return (end->tv_sec-start->tv_sec) + 1e-6*(end->tv_usec-start->tv_usec);
@@ -12,6 +12,43 @@ double tdiff_timeval(struct timeval *start, struct timeval *end) {
 double tdiff_timespec(struct timespec *start, struct timespec *end) {
   return (end->tv_sec-start->tv_sec) + 1e-9*(end->tv_nsec-start->tv_nsec);
 }
+
+class statistics {
+    std::vector<double> values;
+    double min, max;
+  public:
+    statistics() : min(0), max(0) {
+    }
+    void measurement(double f) {
+        if (values.size()==0) {
+            min = max = f;
+        } else {
+            min = std::min(min, f);
+            max = std::max(max, f);
+        }
+        values.push_back(f);
+    }
+    double mean() {
+        double sum = 0;
+        for (size_t i = 0; i < values.size(); i++) {
+            sum+=values[i];
+        }
+        return sum/values.size();
+    }
+    double sigma() {
+        double m = mean();
+        double sum = 0;
+        for (size_t i = 0; i < values.size(); i++) {
+            double diff = values[i]-m;
+            sum += diff*diff;
+        }
+        return sqrt(sum);
+    }
+    void print() {
+        printf("  mean=%12.9fs sigma=%12.9fs max-min/mean=%7.4f%%\n",
+               mean(), sigma(), 100*(max-min)/mean());
+    }
+};
 
 #define N 256
 double x[N];
@@ -43,56 +80,33 @@ int n_trials = 5;
 void timeit_gettimeofday(void) {
     struct timeval start, end;
     printf("gettimeofday:\n");
-    double sum_squares = 0;
-    double sum         = 0;
-    double min = 0, max = 0;
+    statistics stats;
     for (int trial = 0; trial < n_trials; trial++) {
         init();
         gettimeofday(&start, NULL);
         loop();
         gettimeofday(&end,   NULL);
         double tim = tdiff_timeval(&start, &end);
-        sum_squares += tim*tim;
-        sum         += tim;
-        if (trial==0) min = max = tim;
-        else {
-            if (tim < min) min = tim;
-            if (tim > max) max = tim;
-        }
-
+        stats.measurement(tim);
         printf(" %12.9fs\n", tim);
     }
-    double avg_x = sum/n_trials;
-    double avg_xsquared = sum_squares/n_trials;
-    printf("  mean=%12.9fs sqrt(variance)=%12.9fs relative_range=%5.2f\n",
-           avg_x, sqrt(avg_xsquared-avg_x), (max-min)/max);
+    stats.print();
 }
 
 void timeit_clock_gettime(clockid_t clk_id, const char *string) {
     struct timespec start, end;
     printf("%s\n", string);
-    double sum_squares = 0;
-    double sum         = 0;
-    double min = 0, max = 0;
+    statistics stats;
     for (int trial = 0; trial < n_trials; trial++) {
         init();
         clock_gettime(clk_id, &start);
         loop();
         clock_gettime(clk_id, &end);
         double tim = tdiff_timespec(&start, &end);
-        sum_squares += tim*tim;
-        sum         += tim;
-        if (trial==0) min = max = tim;
-        else {
-            if (tim < min) min = tim;
-            if (tim > max) max = tim;
-        }
+        stats.measurement(tim);
         printf(" %12.9fs\n", tim);
     }
-    double avg_x = sum/n_trials;
-    double avg_xsquared = sum_squares/n_trials;
-    printf("  mean=%12.9fs sqrt(variance)=%12.9fs relative_range=%5.2f\n",
-           avg_x, sqrt(avg_xsquared-avg_x), (max-min)/max);
+    stats.print();
 }
 
 #define CGT(n) timeit_clock_gettime(CLOCK_ ## n, #n)
